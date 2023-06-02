0bbc620e45d691bdc5db65e4cb2653969c8f8aa9
nataliazakrevskaya <zakrevskaya.97@inbox.ru>
2023-06-02 16:33:22 +0300

    add changes with default city

--- web/src/components/Common/Modal/DefaultCity/DefaultCity.tsx
+++ web/src/components/Common/Modal/DefaultCity/DefaultCity.tsx
@@ -2,19 +2,28 @@ import React, {useContext} from 'react'
 import {Button} from '../../Button/Button'
 import styles from './DefaultCity.module.scss'
 import {userApi} from '@api/user/user-api'
-import {UserContext} from "../../../../App";
-import {UserContextType} from "../../../../types";
+import {ChosenCityContext, UserContext} from "../../../../App";
+import {ChosenCityContextType, UserContextType} from "../../../../types";
 
 interface ModalPropsType{
   closeModal: () => void
 }
 
 const DefaultCity = ({closeModal}: ModalPropsType) => {
+
+  const {chooseCity} = useContext(ChosenCityContext) as ChosenCityContextType
   const {addUser} = useContext(UserContext) as UserContextType
   const setDefault = async () => {
     await userApi.setDefaultCitySettings()
     await userApi.fetchUser().then(res => {
       addUser(res.data)
+      const data = res.data.towns.find(town => town.city_id === "6478fd9630e79c580489ba43")
+      if(data){
+        chooseCity(data)
+      } else {
+        if(res.data?.towns?.length > 0)
+          chooseCity(res.data.towns[0])
+      }
     })
     closeModal()
   }
